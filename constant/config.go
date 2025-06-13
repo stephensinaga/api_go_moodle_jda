9bc031aa0aa8a17f0fa4125d3cd0cce7aa29b544
stephansinaga <stefansinaga10@gmail.com>
2025-06-13 13:21:40 +0700

    psttt

--- constant/config.go
+++ constant/config.go
@@ -6,7 +6,7 @@ const (
 	MoodleToken       = "6b5893a337b538f2660bfe5d58733db3" // ganti sesuai token yang di beri dari moodle
 	WSFunctionCreate  = "core_user_create_users"  // tidak boleh diganti
 	WSFunctionEnrol   = "enrol_manual_enrol_users"  // tidak boleh diganti
+	HOST              = "192.168.95.68:4500"   // ini sesuai dengan host yang ingin anda buat
 	RoleID			  = 5 // tidak boleh diganti
 	CourseID	      = 3  // ganti sesuai dengan id dari course moodle
-	HOST              = "192.168.95.114:4500"   // ini sesuai dengan host yang ingin anda buat
 )
